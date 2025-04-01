19dfd6a7e7eb283dce74930d1e9e522b74e96f10
Rajveer Singh <raj14.1984@gmail.com>
2025-04-01 21:08:45 +0530

    tracing working for feign client

--- entrypoint-service/src/main/java/com/ksoot/observability/EdgeClient.java
+++ entrypoint-service/src/main/java/com/ksoot/observability/EdgeClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "edge-client", url = "http://localhost:8091", configuration = FeignConfig.class)
+@FeignClient(value = "edge-client", url = "http://localhost:8091")
 public interface EdgeClient {
 
   @RequestMapping(method = RequestMethod.GET, value = "/edge-endpoint")
