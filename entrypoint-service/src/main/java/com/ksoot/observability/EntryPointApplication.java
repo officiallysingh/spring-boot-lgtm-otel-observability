66e73a7b7ba7ed38dcda6987acc690b2211296ae
Rajveer Singh <raj14.1984@gmail.com>
2025-04-01 23:38:34 +0530

    updated

--- entrypoint-service/src/main/java/com/ksoot/observability/EntryPointApplication.java
+++ entrypoint-service/src/main/java/com/ksoot/observability/EntryPointApplication.java
@@ -3,8 +3,6 @@ package com.ksoot.observability;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -13,15 +11,4 @@ public class EntryPointApplication {
   public static void main(String[] args) {
     SpringApplication.run(EntryPointApplication.class, args);
   }
-
-  @Bean
-  public RestTemplate restTemplate() {
-    return new RestTemplate();
-  }
-
-  //  @Bean
-  //  public Capability capability(final MeterRegistry registry) {
-  //    return new MicrometerCapability(registry);
-  //  }
-
 }
